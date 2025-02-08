a4d45a541f863f21a3bc7ba7792cafdf605db44a
Ahsanop2 <alizabutt3257@gmail.com>
2025-02-08 09:31:31 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ function sendToWhatsApp() {
 
     if (level && rank && extra) {
         var message = "Free Fire Account Sale%0A" +
-                      "Level: " + level + "%0A" +
-                      "Rank: " + rank + "%0A" +
-                      "Extra Info: " + extra + "%0A";
+            message = "Full Tersted Free Fire Account Saler Ahsan op%0A" +
+                      "LVL: " + level + "%0A" +
+                      "TOTEL EVOS: " + rank + "%0A" +
+                      "KUCH OR INFO: " + extra + "%0A";
 
         var whatsappNumber = "923182898491"; // Apna WhatsApp number yahan dalain
-        var whatsappURL = "https://wa.me/923182898491" + whatsappNumber + "?text=Hello" + message;
+        var whatsappURL = "https://wa.me/923182898491" + whatsappNumber + "?text=" + message;
 
         window.open(whatsappURL, "_blank");
     } else {
