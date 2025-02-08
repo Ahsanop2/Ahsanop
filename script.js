3ea03c58eb1a9568caa342e53f6ba64ea3f604e9
Ahsanop2 <alizabutt3257@gmail.com>
2025-02-08 09:47:18 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ function sendToWhatsApp() {
     var level = document.getElementById("level").value;
     var rank = document.getElementById("rank").value;
     var extra = document.getElementById("extra").value;
+    var image = document.getElementById("image").value;
 
-    if (level && rank && extra) {
+    if (level && rank && extra && image) {
         var message = "Free Fire Account Sale%0A" +
-            message = "Full Tersted Free Fire Account Saler Ahsan op%0A" +
-                      "LVL: " + level + "%0A" +
-                      "TOTEL EVOS: " + rank + "%0A" +
-                      "KUCH OR INFO: " + extra + "%0A";
- 
+                      "Level: " + level + "%0A" +
+                      "Evos: " + rank + "%0A" +
+                      "Extra Info: " + extra + "%0A" +
+                      "Image Link: " + image + "%0A";
+
         var whatsappNumber = "923182898491"; // Apna WhatsApp number likhein
         var whatsappURL = "https://wa.me/" + whatsappNumber + "?text=" + message;
 
