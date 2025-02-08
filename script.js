79c6c6c74abd1f7084d6bf82386ef918ddbd0e4c
Ahsanop2 <alizabutt3257@gmail.com>
2025-02-08 09:13:57 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-document.getElementById('imageInput').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    
-    if (file) {
-        const reader = new FileReader();
-        
-        reader.onload = function(e) {
-            const imagePreview = document.getElementById('imagePreview');
-            imagePreview.src = e.target.result;
-            imagePreview.style.display = 'block'; // Show the preview
-        };
-        
-        reader.readAsDataURL(file);
+function sendToWhatsApp() {
+    var level = document.getElementById("level").value;
+    var rank = document.getElementById("rank").value;
+    var extra = document.getElementById("extra").value;
+
+    if (level && rank && extra) {
+        var message = "Free Fire Account Sale%0A" +
+                      "Level: " + level + "%0A" +
+                      "Rank: " + rank + "%0A" +
+                      "Extra Info: " + extra + "%0A";
+
+        var whatsappNumber = "923001234567"; // Apna WhatsApp number yahan dalain
+        var whatsappURL = "https://wa.me923182898491/" + whatsappNumber + "?text=" + message;
+
+        window.open(whatsappURL, "_blank");
+    } else {
+        alert("Please fill all fields!");
     }
-});
+}
