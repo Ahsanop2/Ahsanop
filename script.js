b8a73e259ad6feea0c2eb657b89d047ae5603d16
Ahsanop2 <alizabutt3257@gmail.com>
2025-02-08 08:58:49 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('imageInput').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    
+    if (file) {
+        const reader = new FileReader();
+        
+        reader.onload = function(e) {
+            const imagePreview = document.getElementById('imagePreview');
+            imagePreview.src = e.target.result;
+            imagePreview.style.display = 'block'; // Show the preview
+        };
+        
+        reader.readAsDataURL(file);
+    }
+});
