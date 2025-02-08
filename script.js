bf7462932b64786aaeaee249e6ccf10af44dfe03
Ahsanop2 <alizabutt3257@gmail.com>
2025-02-08 10:38:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Function to send WhatsApp message when buying an ID
 function buyNow(number) {
-    window.open(`https://wa.me923182898491/${number}?text=I%20want%20to%20buy%20a%20Free%20Fire%20ID`, '_blank');
+    window.open(`https://wa.me923182898491/${923182898491}?text=I%20want%20to%20buy%20a%20Free%20Fire%20ID`, '_blank');
 }
 
 // Handling the Sell Form Submission
